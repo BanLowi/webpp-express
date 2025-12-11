c7cebf296233a9403d189bfb4180dffd9ec96faf
BanLowi <lowi40k@gmail.com>
2025-12-11 18:11:01 +0100

    added cors for localhost

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const cors = require("cors");
 const moviesRouter = require("./routers/movies");
 
 app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 
 app.listen(PORT, () => {
     console.log(`Listening to port: ${PORT}`);
